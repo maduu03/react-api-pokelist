30de73a66c1b97c84977cc8c7cdbf79f00086910
maduu03 <madu.gms03@gmail.com>
2022-12-16 22:19:04 -0300

    Atualizando

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react'
+import { SafeAreaView, StatusBar,StyleSheet, FlatList, View, Image, Text } from 'react-native'
 
-export default function App() {
+export default function App () {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      setPokemons(data.results)
+    })
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView>
+      <View style={{top:50}}>
+        <Text>+++++++ Veja abaixo os 15 Pokemons principais +++++++</Text>
+      </View>
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon) => pokemon.name}
+        contentContainerStyle={{flexGrow: 1}}
+        renderItem={PokemonShow}
+      />
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PokemonShow(item) {
+
+  const { name, url } = item.item
+
+  const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/','')
+
+  const imageUrl = 'https://pokeres.bastionbot.org/images/pokemon/'+pokemonNumber+'.png'
+
+  return (
+    <View style={{ flexDirection: 'column', top: 94 }}>
+      <Text>+ {name}</Text>
+    </View>
+  )
+}
